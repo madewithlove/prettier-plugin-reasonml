c72166ed6030bba6846dd184468fec1ff0e4b6f3
Maxime Fabre <ehtnam6@gmail.com>
2018-11-30 15:53:25 +0100

    Add scripts

--- src/parse.js
+++ src/parse.js
@@ -1,11 +1,11 @@
 const { format } = require("./utils");
 
 module.exports = function parse(text, parsers, opts) {
-    return {
-        ast_type: "refmt",
-        body: format(text),
-        end: text.length,
-        source: text,
-        start: 0,
-    };
+  return {
+    ast_type: "refmt",
+    body: format(text),
+    end: text.length,
+    source: text,
+    start: 0
+  };
 };
